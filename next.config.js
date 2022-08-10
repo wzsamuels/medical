1f8ea340ee8825fcf36f782d7b1f9667ced60a3e
wzsamuels <wzsamuels@gmail.com>
2022-08-10 15:04:33 -0400

    Added env variable names to next config for amplify.

--- next.config.js
+++ next.config.js
@@ -2,16 +2,20 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY
+  },
   images: {
-    domains: ['images.pexels.com', 'twinsilver.mo.cloudinary.net'],
+    domains: ['images.pexels.com', 'twinsilver.mo.cloudinary.net']
   },
   experimental: {
     images: {
-      layoutRaw: true,
-    },
+      layoutRaw: true
+    }
   },
   compiler: {
-    styledComponents: true,
+    styledComponents: true
     /*
     removeConsole: {
       exclude: ['error'],
